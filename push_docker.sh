614cef3b6d9247924d6d4ff8c0c42d79e1d5f82f
marekchen <marek@163.com>
2018-08-02 15:29:41 +0800

    update

--- push_docker.sh
+++ push_docker.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-echo "$CIRRUS_BRANCH"
-
-if [ "$CIRRUS_BRANCH" != "master" ]
-then
-    exit 0
-fi
-
 docker login --username $DOCKER_USER_NAME --password $DOCKER_PASSWORD
 
 # docker push marekchen/flutter:base
