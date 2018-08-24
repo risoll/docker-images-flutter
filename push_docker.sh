aaefbbe6c428b5076b09c9c6909bef2483ea220c
marekchen <marek@163.com>
2018-08-24 21:07:27 +0800

    0.7.0

--- push_docker.sh
+++ push_docker.sh
@@ -5,5 +5,5 @@ set -e
 docker login --username $DOCKER_USER_NAME --password $DOCKER_PASSWORD
 
 # docker push marekchen/flutter:base
-docker push marekchen/flutter:0.5.7
+docker push marekchen/flutter:0.7.0 
 docker push marekchen/flutter:latest
