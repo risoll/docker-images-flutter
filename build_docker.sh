108bcaa0a030bc16f1372c70689926fceff50c66
risoll <rzkysol@gmail.com>
2018-11-11 13:43:33 +0700

    revert docker pull

--- build_docker.sh
+++ build_docker.sh
@@ -3,7 +3,7 @@
 set -e
 
 # pull to make sure we are not rebuilding for nothing
-docker pull risol/flutter:base
+# docker pull risol/flutter:base
 
 # docker build --tag risol/flutter:base base
 docker build --tag risol/flutter:0.10.2  --tag risol/flutter:latest ./
